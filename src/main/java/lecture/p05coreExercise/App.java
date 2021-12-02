60ee7b85c37963838450458ef7e8b0cf10890f2f
sebaek <sebaek@gmail.com>
2021-12-02 16:30:36 +0900

    dependency injection, inversion of control

--- src/main/java/lecture/p05coreExercise/App.java
+++ src/main/java/lecture/p05coreExercise/App.java
@@ -1,5 +1,23 @@
 package lecture.p05coreExercise;
 
-public class App {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class App {
+	public static void main(String[] args) {
+		
+		String configLocation = "lecture/p05coreExercise/context.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(configLocation);
+		
+		Computer c = context.getBean(Computer.class);
+//		context.getBean(Memory.class);
+		Memory m = (Memory) context.getBean("memory");
+		
+		log.warn(c);
+		log.warn(m);
+		log.warn(c.getMemory());
+	}
 }
