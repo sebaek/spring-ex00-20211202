58f7ed4fb991608dade0a41a4daf7ad262cfc88d
sebaek <sebaek@gmail.com>
2021-12-03 16:27:23 +0900

    @requestparam

--- src/main/java/org/zerock/controller/p01controller/Controller03.java
+++ src/main/java/org/zerock/controller/p01controller/Controller03.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/cont03")
@@ -29,7 +30,72 @@ public class Controller03 {
 		System.out.println(name);
 		System.out.println(age);
 	}
+	
+	// /cont03/met03 : request 파라미터, address(String)와 id(Integer)를 받도록 메소드 작성
+	@RequestMapping("/met03")
+	public void method03(String address, Integer id) {
+		System.out.println(address);
+		System.out.println(id);
+	}
+	
+	
+	
+	@RequestMapping("/met04")
+	public void method04(@RequestParam("name") String contactName) {
+		System.out.println(contactName);
+	}
+	
+	
+	// url:/cont03/met05?country=korea&height=200 
+	@RequestMapping("/met05")
+	public void method05(String c, Integer h) {
+		System.out.println(c);
+		System.out.println(h);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
