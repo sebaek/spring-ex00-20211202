0f897e1c81f560f3e5d4c500ad7bc7e093f91a47
sebaek <sebaek@gmail.com>
2021-12-03 14:36:45 +0900

    requestmapping

--- src/main/java/org/zerock/controller/p01controller/Controller01.java
+++ src/main/java/org/zerock/controller/p01controller/Controller01.java
@@ -6,12 +6,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cont01")
 public class Controller01 {
-
+	
+	// "/cont01"
+	@RequestMapping("")
+	public void method00() {
+		System.out.println("메소드00 일함");
+	}
+	
+	// "/cont01/met01"
 	@RequestMapping("/met01")
 	public void method01() {
 		System.out.println("메소드01 일함");
 	}
 	
 	// "/cont01/met02" 요청이 왔을 때 일하는 메소드(method02)를 작성하세요. return : void
+	@RequestMapping("/met02")
+	public void method02() {
+		System.out.println("메소드02 일함");
+	}
+	
+	
+	// "/cont01/met03", "/cont01/met04" 로 요청이 왔을 때 일하는 메소드
+	@RequestMapping({"/met03", "/met04"})
+	public void method03() {
+		System.out.println("메소드 03 일함");
+	}
+	
+	// "/cont01/met05", "/cont01/met06" 로 요청이 왔을 때 일하는 메소드 method05 작성
 	
 }
+
+
+
+
+
+
+
