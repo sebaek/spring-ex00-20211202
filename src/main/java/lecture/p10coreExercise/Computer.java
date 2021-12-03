11ec25afdf8b5ae8f8073421647a295a646511d6
sebaek <sebaek@gmail.com>
2021-12-03 11:17:23 +0900

    component scan, dependency injection, lombok

--- src/main/java/lecture/p10coreExercise/Computer.java
+++ src/main/java/lecture/p10coreExercise/Computer.java
@@ -1,5 +1,24 @@
 package lecture.p10coreExercise;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter(onMethod_ = @Autowired)
 public class Computer {
 
+	private Memory memory;
+
+//	@Autowired
+//	public void setMemory(Memory memory) {
+//		this.memory = memory;
+//	}
+//
+//	public Memory getMemory() {
+//		return memory;
+//	}
 }
