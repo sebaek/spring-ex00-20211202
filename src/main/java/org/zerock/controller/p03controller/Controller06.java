8eb210f93ba44b382b84d6db5a32b883348a10fe
sebaek <sebaek@gmail.com>
2021-12-06 16:07:27 +0900

    modelattribute

--- src/main/java/org/zerock/controller/p03controller/Controller06.java
+++ src/main/java/org/zerock/controller/p03controller/Controller06.java
@@ -21,112 +21,87 @@ public class Controller06 {
 	public void method02(Model model) {
 		// 4. add attribute
 		model.addAttribute("myattr1", "myvalue1");
-		
+
 		// 5
 	}
-	
-	// 할일 : 요청 경로 : /cont06/met03 
-	//        view name : /cont06/met03 
-	//        view에서 사용하는 attribute 명 : "myattr2"
-	//                          myattr2의 값 : "myvalue2"
+
+	// 할일 : 요청 경로 : /cont06/met03
+	// view name : /cont06/met03
+	// view에서 사용하는 attribute 명 : "myattr2"
+	// myattr2의 값 : "myvalue2"
 	// 가 되도록 method03 작성
 	// 또한 view도 작성해서 myattr2가 myvalue2를 출력하는 확인
-	
+
 	@RequestMapping("/met03")
 	public void method03(Model model) {
-		
+
 		model.addAttribute("myattr2", "myvalue2");
-		
+
 	}
-	
+
 	@RequestMapping("/met04")
 	public void method04(Model model) {
-		// 3. 
+		// 3.
 		Bean01 bean = new Bean01();
 		bean.setAge(99);
 		bean.setName("trump");
-		
-		// 4. 
+
+		// 4.
 		model.addAttribute("myBean", bean);
 	}
-	
+
 	@RequestMapping("/met05")
 	public void method05(Model model) {
 		// 3.
 		Bean01 bean = new Bean01();
 		bean.setAge(88);
 		bean.setName("donald");
-		
+
 		// 4.
-		//model.addAttribute("bean01", bean); // attribute의 이름이 클래스(type)의 이름(to lower camel case)과 같은 경우 생략 가능
+		// model.addAttribute("bean01", bean); // attribute의 이름이 클래스(type)의 이름(to lower
+		// camel case)과 같은 경우 생략 가능
 		model.addAttribute(bean);
-		
-		// 할일 : view (jsp) 를 만들어서 "bean01" attribute가 있는 지 확인) 
+
+		// 할일 : view (jsp) 를 만들어서 "bean01" attribute가 있는 지 확인)
 	}
-	
+
 	@RequestMapping("/met06")
 	public void method06(Model model) {
-		
+
 		model.addAttribute("attr1", "val1");
 		model.addAttribute("attr2", "val2");
-		
+
 	}
-	
+
 	@RequestMapping("/met07")
 	public void method07(String name, String city, Model model) {
-		
+
 		model.addAttribute("name", name);
 		model.addAttribute("city", city);
-		
+
 	}
-	
-	
+
 	@RequestMapping("/met08")
-	public String method08(@ModelAttribute("name") String name, 
+	public String method08(@ModelAttribute("name") String name,
 			@ModelAttribute("city") String city) {
-		
+
 		return "/cont06/met07";
 	}
+
+	@RequestMapping("/met09")
+	public void method09(Bean01 bean, Model model) {
+		
+		model.addAttribute("bean", bean);
+		
+	}
+	
+	@RequestMapping("/met10")
+	public String method10(@ModelAttribute("bean") Bean01 bean) {
+		
+		return "/cont06/met09";
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
