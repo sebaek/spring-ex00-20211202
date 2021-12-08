6a6b8911f9b31d8fa73be3b06b43d160b4393e5b
sebaek <sebaek@gmail.com>
2021-12-08 12:23:01 +0900

    new select

--- src/main/java/org/zerock/controller/p05controller/Controller10.java
+++ src/main/java/org/zerock/controller/p05controller/Controller10.java
@@ -18,6 +18,16 @@ public class Controller10 {
 	public void method01() {
 		System.out.println(mapper.getSupplierOne());
 	}
+	
+	@RequestMapping("/met02")
+	public void method02() {
+		// SELECT LastName, FirstName FROM Employees WHERE EmployeeID = 3
+		// Mapper01.java에 getEmployeeNameOne 메소드 작성
+		//                                   위 메소드 리턴 타입 Bean09 사용
+		// Mapper01.xml 에 새 <select> 요소 작성
+		
+		// 이 메소드에서 getEmployeeNameOne 메소드 실행 결과(Bean09) 출력
+	}
 }
 
 
