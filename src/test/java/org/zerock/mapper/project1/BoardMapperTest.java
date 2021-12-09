b40322cc09f1dca6dd94c12cf0f7dc17c79fea81
sebaek <sebaek@gmail.com>
2021-12-09 16:32:24 +0900

    root context

---
+++ src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
@@ -0,0 +1,22 @@
+package org.zerock.mapper.project1;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BoardMapperTest {
+	
+	@Autowired
+	public BoardMapper mapper;
+
+	@Test
+	public void mapperTest() {
+		assertNotNull(mapper);
+	}
+}
