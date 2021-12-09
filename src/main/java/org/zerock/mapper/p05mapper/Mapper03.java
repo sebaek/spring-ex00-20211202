044d85aa3f9c1a749aa25a99e3dcf9b812d31726
sebaek <sebaek@gmail.com>
2021-12-09 10:07:05 +0900

    insert supplier

--- src/main/java/org/zerock/mapper/p05mapper/Mapper03.java
+++ src/main/java/org/zerock/mapper/p05mapper/Mapper03.java
@@ -6,4 +6,7 @@ public interface Mapper03 {
 
 	public int insertCustomer(@Param("customerName") String customerName,
 			@Param("contactName") String contactName);
+
+	public int insertSupplier(@Param("supplierName") String supplierName, 
+			@Param("contactName") String contactName);
 }
