ab37d7e991532dc8a71d0767325a07e349156f12
sebaek <sebaek@gmail.com>
2021-12-09 15:34:51 +0900

    mapper

---
+++ src/main/webapp/WEB-INF/sql/01board.sql
@@ -0,0 +1,11 @@
+USE test;
+CREATE TABLE Board (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(200) NOT NULL,
+    content VARCHAR(2000) NOT NULL,
+    writer VARCHAR(50) NOT NULL,
+    inserted DATETIME DEFAULT NOW(),
+    updated DATETIME DEFAULT NOW()
+);
+
+DESC Board;
