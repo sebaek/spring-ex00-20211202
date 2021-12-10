6ce23b1ef7ce5603d3b084033821705bcb84e38e
sebaek <sebaek@gmail.com>
2021-12-10 14:25:10 +0900

    register

--- src/main/java/org/zerock/controller/project1/BoardController.java
+++ src/main/java/org/zerock/controller/project1/BoardController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.project1.BoardVO;
 import org.zerock.service.project1.BoardService;
 
@@ -40,7 +42,40 @@ public class BoardController {
 		
 		model.addAttribute("board", board);
 	}
+	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		// 3. business logic
+		service.register(board);
+		
+		// 4. add attribute
+		rttr.addAttribute("result", board.getId());
+		
+		// 5. forward / redirect
+		// 책: 목록으로 redirect
+		return "redirect:/board/list";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
