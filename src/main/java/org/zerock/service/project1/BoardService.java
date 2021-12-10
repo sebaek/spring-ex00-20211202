1ccbc285003b43dec4f2154d96c1376e758776f3
sebaek <sebaek@gmail.com>
2021-12-10 11:13:21 +0900

    service

--- src/main/java/org/zerock/service/project1/BoardService.java
+++ src/main/java/org/zerock/service/project1/BoardService.java
@@ -1,7 +1,10 @@
 package org.zerock.service.project1;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zerock.domain.project1.BoardVO;
 import org.zerock.mapper.project1.BoardMapper;
 
 import lombok.Setter;
@@ -13,4 +16,37 @@ public class BoardService {
 	private BoardMapper mapper;
 	
 	
+	public boolean register(BoardVO board) {
+		return mapper.insert(board) == 1;
+	}
+	
+	public BoardVO get(Integer id) {
+		return mapper.read(id);
+	}
+	
+	public boolean modify(BoardVO board) {
+		return mapper.update(board) == 1;
+	}
+	
+	public boolean remove(Integer id) {
+		return mapper.delete(id) == 1;
+	}
+	
+	public List<BoardVO> getList() {
+		return mapper.getList();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
