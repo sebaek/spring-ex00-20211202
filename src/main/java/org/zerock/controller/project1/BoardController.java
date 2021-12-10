30a0f780cfbd0fa677d1539a985d1f82450cb6f7
sebaek <sebaek@gmail.com>
2021-12-10 12:13:32 +0900

    get

--- src/main/java/org/zerock/controller/project1/BoardController.java
+++ src/main/java/org/zerock/controller/project1/BoardController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.project1.BoardVO;
 import org.zerock.service.project1.BoardService;
 
@@ -18,7 +20,7 @@ public class BoardController {
 	@Setter(onMethod_ = @Autowired)
 	private BoardService service;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public void list(Model model) {
 		// 3. business logic
 		// 게시물(Board) 목록 조회
@@ -29,6 +31,14 @@ public class BoardController {
 		
 		// 5. forward / redirect
 		// jsp path : /WEB-INF/views/board/list.jsp
+	}
+	
+	// /board/get?id=10
+	@GetMapping("/get")
+	public void get(@RequestParam("id") Integer id, Model model) {
+		BoardVO board = service.get(id);
+		
+		model.addAttribute("board", board);
 	}
 }
 
