911c4e3d372d1434e3d9fd89d6469426f6965ac7
sebaek <sebaek@gmail.com>
2021-12-10 10:28:07 +0900

    list test

--- src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
+++ src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -103,6 +106,32 @@ public class BoardMapperTest {
 		assertNull(deleted);
 	}
 	
+	@Test
+	public void getListTest() {
+		List<BoardVO> list = mapper.getList();
+		assertNotNull(list);
+		assertTrue(list.size() > 0);
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("list 테스트용 ");
+		vo.setContent("list 테스트용 본문");
+		vo.setWriter("tester");
+		
+		mapper.insert(vo);
+		
+		List<BoardVO> list2 = mapper.getList();
+		
+		assertEquals(list.size() + 1, list2.size());
+		
+		for (BoardVO item : list2) {
+			assertNotNull(item.getId());
+			assertNotNull(item.getTitle());
+			assertNotNull(item.getContent());
+			assertNotNull(item.getWriter());
+			assertNotNull(item.getInserted());
+			assertNotNull(item.getUpdated());
+		}
+	}
 
 }
 
