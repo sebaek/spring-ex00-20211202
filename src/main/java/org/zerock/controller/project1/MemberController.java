11f92f59c2e10943f99c6c7d373be5afb35f43aa
sebaek <sebaek@gmail.com>
2021-12-13 16:06:55 +0900

    modify info

--- src/main/java/org/zerock/controller/project1/MemberController.java
+++ src/main/java/org/zerock/controller/project1/MemberController.java
@@ -18,22 +18,22 @@ import lombok.Setter;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	@Setter(onMethod_ = @Autowired)
 	private MemberService service;
 
 	@GetMapping("/signup")
 	public void signup() {
-		
+
 	}
-	
+
 	@PostMapping("/signup")
 	public String signup(@ModelAttribute("member") MemberVO member, RedirectAttributes rttr, Model model) {
 		MemberVO m = service.read(member.getId());
-				
+
 		if (m == null) {
 			boolean ok = service.register(member);
-			
+
 			if (ok) {
 				rttr.addFlashAttribute("result", "회원가입이 완료되었습니다.");
 				return "redirect:/board/list";
@@ -44,68 +44,86 @@ public class MemberController {
 			model.addAttribute("alertMessage", "중복된 아이디 입니다.");
 			return null;
 		}
-		
+
 	}
-	
+
 	@GetMapping("/login")
 	public void login() {
-		
+
 	}
-	
+
 	@PostMapping("/login")
 	public String login(String id, String password, HttpSession session) {
 		// service 사용해서 아이디로 멤버vo 얻고
 		MemberVO vo = service.read(id);
-		
+
 		if (vo == null) {
 			return null;
 		}
-		
+
 		// 얻어온 멤버vo의 패스워드와 입력한 패스워드가 같은 지 확인
 		boolean correctPassword = password.equals(vo.getPassword());
-		
+
 		// 멤버vo가 널이거나 패스워드가 다르면 로그인 실패
 		if (!correctPassword) {
 			return null;
 		}
-		
+
 		// 멤버vo가 널이 아니고 패스워드가 일치하면 로그인 성공
 		// 로그인 성공
 		session.setAttribute("loggedInMember", vo);
-		
+
 		System.out.println(session.getAttribute("loggedInMember"));
-		
+
 		return "redirect:/board/list";
-		
+
 	}
-	
-	@RequestMapping("/logout") 
+
+	@RequestMapping("/logout")
 	public String logout(HttpSession session) {
 		// 세션 invalidate
 		session.invalidate();
 		// /board/list redirect
 		return "redirect:/board/list";
 	}
-	
+
 	@GetMapping("/info")
 	public String info(HttpSession session) {
 		MemberVO vo = (MemberVO) session.getAttribute("loggedInMember");
-		
+
+		// 로그아웃 상태
+		if (vo == null) {
+			return "redirect:/member/login";
+		}
+
+		// 로그인 상태일 때
+		return null;
+	}
+
+	@PostMapping("/info")
+	public String info(MemberVO member, HttpSession session, RedirectAttributes rttr) {
+		MemberVO vo = (MemberVO) session.getAttribute("loggedInMember");
+
 		// 로그아웃 상태
 		if (vo == null) {
 			return "redirect:/member/login";
 		}
 		
-		// 로그인 상태일 때
-		return null;
-	}
-	
-	@PostMapping("/info")
-	public String info(MemberVO vo, HttpSession session) {
+		// 로그인된 상태
+		boolean ok = service.modify(member);
 		
-		return null;
+		if (ok) {
+			rttr.addFlashAttribute("result", "회원 정보가 변경되었습니다.");
+			session.setAttribute("loggedInMember", service.read(member.getId()));
+		} else {
+			rttr.addFlashAttribute("result", "회원 정보가 변경되지 않았습니다.");
+		}
+
+		return "redirect:/board/list";
 	}
 }
+
+
 
 
 
