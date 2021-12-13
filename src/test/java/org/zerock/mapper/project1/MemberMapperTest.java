f118cc182be00d201610dc115f8205e251a827b0
sebaek <sebaek@gmail.com>
2021-12-13 11:06:05 +0900

    update test

--- src/test/java/org/zerock/mapper/project1/MemberMapperTest.java
+++ src/test/java/org/zerock/mapper/project1/MemberMapperTest.java
@@ -2,6 +2,9 @@ package org.zerock.mapper.project1;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +20,51 @@ public class MemberMapperTest {
 	public MemberMapper mapper;
 
 	@Test
-	public void insertTest() {
+	public void mapperTest() {
+		String id = "member" + (new Date()).getTime();
+		String password = "newpasswrd" + (new Date()).getTime();
+		String address = "jeju" + (new Date()).getTime();
+		String email = id + "@gmail.com";
+
 		MemberVO vo = new MemberVO();
-		vo.setId("newMember1");
-		vo.setPassword("newpassword");
-		vo.setAddress("seoul");
-		vo.setEmail("member1@google.com");
+		vo.setId(id);
+		vo.setPassword(password);
+		vo.setAddress(address);
+		vo.setEmail(email);
 		
+		// insert test
 		int cnt = mapper.insert(vo);
-		
 		assertEquals(1, cnt);
+
+		// select test
+		MemberVO s = mapper.select(id);
+		
+		assertEquals(id, s.getId());
+		assertEquals(password, s.getPassword());
+		assertEquals(address, s.getAddress());
+		assertEquals(email, s.getEmail());
+		
+		// update test
+		String newPassword = "newPassword";
+		String newAddress = "newAddress";
+		LocalDateTime inserted = s.getInserted(); 
+		
+		s.setPassword(newPassword);
+		s.setAddress(newAddress);
+		
+		cnt = mapper.update(s);
+		assertEquals(1, cnt);
+		
+		MemberVO t = mapper.select(id);
+		assertEquals(id, t.getId());
+		assertEquals(newPassword, t.getPassword());
+		assertEquals(newAddress, t.getAddress());
+		assertEquals(email, t.getEmail());
+		assertEquals(inserted, t.getInserted());
+		
 	}
+
 }
-
-
 
 
 
