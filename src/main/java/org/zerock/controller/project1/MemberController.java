ba12ed4bc692a3d356550ade8ec14c4c2778124c
sebaek <sebaek@gmail.com>
2021-12-13 14:45:10 +0900

    login

--- src/main/java/org/zerock/controller/project1/MemberController.java
+++ src/main/java/org/zerock/controller/project1/MemberController.java
@@ -49,6 +49,28 @@ public class MemberController {
 	public void login() {
 		
 	}
+	
+	@PostMapping("/login")
+	public void login(String id, String password) {
+		// service 사용해서 아이디로 멤버vo 얻고
+		MemberVO vo = service.read(id);
+		
+		if (vo == null) {
+			// 로그인 실패
+		}
+		
+		
+		// 얻어온 멤버vo의 패스워드와 입력한 패스워드가 같은 지 확인
+		boolean correctPassword = password.equals(vo.getPassword());
+		
+		// 멤버vo가 널이거나 패스워드가 다르면 로그인 실패
+		if (!correctPassword) {
+			// 로그인 실패
+		}
+		
+		// 멤버vo가 널이 아니고 패스워드가 일치하면 로그인 성공
+		// 로그인 성공
+	}
 }
 
 
