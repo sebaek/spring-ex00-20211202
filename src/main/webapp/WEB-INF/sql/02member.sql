26aaead76743d6ee1d2ad0bc82eacb18e3d641af
sebaek <sebaek@gmail.com>
2021-12-13 09:19:42 +0900

    member table sql

---
+++ src/main/webapp/WEB-INF/sql/02member.sql
@@ -0,0 +1,10 @@
+USE test;
+
+CREATE TABLE Member (
+	id VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM Member;
