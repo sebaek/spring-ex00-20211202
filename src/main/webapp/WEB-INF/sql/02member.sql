b9189f0b1b800317f28707d15bb0f430e70e9ad1
sebaek <sebaek@gmail.com>
2021-12-14 10:08:34 +0900

    add nickname column

--- src/main/webapp/WEB-INF/sql/02member.sql
+++ src/main/webapp/WEB-INF/sql/02member.sql
@@ -11,3 +11,13 @@ DESC Member;
 ALTER TABLE Member ADD COLUMN inserted DATETIME NOT NULL DEFAULT NOW();
 
 SELECT * FROM Member ORDER BY inserted DESC;
+
+ALTER TABLE Member ADD COLUMN nickName VARCHAR(30); -- 컬럼 추가
+
+UPDATE Member SET nickName = id; -- id 컬럼을 nickName 컬럼으로 추가 (disable safe update)
+
+ALTER TABLE Member MODIFY COLUMN nickName VARCHAR(30) UNIQUE NOT NULL; -- 제약 사항 추가 unique, not null
+
+
+
+
