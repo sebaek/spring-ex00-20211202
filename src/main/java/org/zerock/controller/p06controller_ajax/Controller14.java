6250cc2d2ed49f9952dc5b757cc1b1c60f483bbd
sebaek <sebaek@gmail.com>
2021-12-17 11:32:19 +0900

    send data

---
+++ src/main/java/org/zerock/controller/p06controller_ajax/Controller14.java
@@ -0,0 +1,20 @@
+package org.zerock.controller.p06controller_ajax;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/cont14")
+public class Controller14 {
+
+	@RequestMapping("/met01")
+	@ResponseBody
+	public String method01(@RequestParam("name") String name) {
+		System.out.println("cont14 met01 일함");
+		System.out.println(name);
+		
+		return "hello";
+	}
+}
