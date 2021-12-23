ff3244cc091fcf7b7e369478adfe48cede72baff
sebaek <sebaek@gmail.com>
2021-12-23 09:38:56 +0900

    file upload

--- src/main/java/org/zerock/controller/p09controller/Controller19.java
+++ src/main/java/org/zerock/controller/p09controller/Controller19.java
@@ -1,5 +1,8 @@
 package org.zerock.controller.p09controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,8 +19,32 @@ public class Controller19 {
 		
 		if (file1 != null) {
 			System.out.println(file1.getOriginalFilename());
+			
+			String path = "C:\\Users\\user\\Desktop\\" + file1.getOriginalFilename();
+			File dest = new File(path);
+			
+			try {
+				file1.transferTo(dest);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		return null;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
