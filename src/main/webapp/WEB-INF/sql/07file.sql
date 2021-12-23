f7b1466bbc3db6362e3abf623182210380dfcd48
sebaek <sebaek@gmail.com>
2021-12-23 11:04:51 +0900

    create table File

---
+++ src/main/webapp/WEB-INF/sql/07file.sql
@@ -0,0 +1,10 @@
+USE test;
+
+CREATE TABLE File (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	boardId INT NOT NULL,
+    fileName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (boardId) REFERENCES Board(id)
+);
+
+DESC File;
